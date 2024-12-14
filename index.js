643c124852b4cb32c6a04db0c439800d3e7f0fcb
kayla-sison-126 <kayla.sison126@gmail.com>
2024-12-14 16:42:45 -0600

    Home Pg Progress

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//
+//gets the time and date
+
+function updateTime() {
+    const curr = new Date();
+    let hours = curr.getHours();
+    let mins = curr.getMinutes();
+    let month = curr.getMonth() + 1;
+    let day = curr.getDate();
+    let amOrPm = "AM";
+
+    if (hours > 12) { // formatting for hrs
+        hours-= 12;
+        amOrPm = "PM";
+    }
+    mins = mins < 10 ? '0' + mins : mins; // formatting for minutes
+
+    const timeAsString = `${month}/${day}/1999 ${hours}:${mins}` + " " + amOrPm; // create the string for the time
+    document.getElementById("time").innerHTML = timeAsString; // display the string
+}
+
+
+setInterval(updateTime, 1000); // update time
+updateTime(); // run fct immediately
